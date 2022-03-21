d7d86fd7ce218b9b36338155b85a1d74112499c9
madhuarun <101954892+madhuarun@users.noreply.github.com>
2022-03-21 15:19:25 -0400

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public int add(int n1, int n2,int n3) {
+		return n1+n2+n3;
+	}
+	
+	public int sub(int n1, int n2) {
+		return n1 - n2;
+	}
+	public double multiply(double n1, double n2) {
+		return n1 * n2;
+	}
+	public float modulo(float n1, float n2) {
+		
+		return n1%n2;
+		
+	}
+public float divide(float n1, float n2) {
+		
+		return n1/n2;
+}
+	
+}
